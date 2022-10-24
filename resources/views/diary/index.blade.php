afc755c16970b470d2c069298cdbfa32aed026b0
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-10-25 08:15:53 +0900

    デザイン微修正

--- resources/views/diary/index.blade.php
+++ resources/views/diary/index.blade.php
@@ -15,14 +15,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
                     <section class="text-gray-600 body-font overflow-hidden">
                         <div class="container px-5 py-4 mx-auto">
                             <div class="flex flex-wrap -m-4">
                                 @foreach ($diaries as $diary)
-                                    <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+                                    <div class="p-4 xl:w-1/4 md:w-1/2 w-full cursor-pointer "s
+                                        onclick="location.href='/diary/detail/{{ $diary->id }}'">
                                         <div
-                                            class="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
+                                            class="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden hover:bg-indigo-100">
                                             <span
                                                 class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
                                                 {{ App\Models\Diary::DIARY_STATUS_OBJECT[$diary->importance] }}
@@ -30,9 +30,9 @@
                                             <h2 class="text-sm tracking-widest title-font mb-1 font-medium">
                                                 {{ Carbon\Carbon::parse($diary->date)->format('Y年n月j日') }} /
                                                 {{ Carbon\Carbon::parse($diary->time)->format('H:i') }}</h2>
-                                            <span
-                                                class="text-m text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">{{ Str::limit($diary->coping_measures, 20, '...') }}</span>
-
+                                            <h1
+                                                class="text-lg text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+                                                {{ Str::limit($diary->coping_measures, 20, '...') }}</h1>
                                             <p class="flex items-center text-gray-600 mb-2">
                                                 <span
                                                     class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -51,23 +51,23 @@
                                                         viewBox="0 0 24 24">
                                                         <path d="M20 6L9 17l-5-5"></path>
                                                     </svg>
-                                                </span>{{ Str::limit($diary->coping_measures, 20, '...') }}
+                                                </span>{{ Str::limit($diary->feeling, 20, '...') }}
                                             </p>
-                                            <button onclick="location.href='/diary/detail/{{ $diary->id }}'"
+                                            {{-- <button onclick="location.href='/diary/detail/{{ $diary->id }}'"
                                                 class="flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">詳細
                                                 <svg fill="none" stroke="currentColor" stroke-linecap="round"
                                                     stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto"
                                                     viewBox="0 0 24 24">
                                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                                 </svg>
-                                            </button>
+                                            </button> --}}
                                         </div>
                                     </div>
                                 @endforeach
                             </div>
                         </div>
                     </section>
-
+                    </a>
                     {{ $diaries->links() }}
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard')->middleware('verified');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard')->middleware('verified');
 
 
 Route::middleware(['auth'])->prefix('diary')->group(function () {
