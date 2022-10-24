b6e6d46135f71583a583ad4b096437db5f010f87
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-10-25 08:21:19 +0900

    routing綺麗に。

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard')->middleware('verified');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard')->middleware('verified');
 
 
-Route::middleware(['auth'])->prefix('diary')->group(function () {
-    Route::get('/', [DiaryController::class, 'index'])->name('diary');
-    Route::get('/create', [DiaryController::class, 'create'])->name('diary.create');
+Route::middleware(['auth'])->prefix('diary')->controller(DiaryController::class)->group(function () {
+    Route::get('/', 'index')->name('diary');
+    Route::get('/create', 'create')->name('diary.create');
 
-    Route::get('/detail/{id}', [DiaryController::class, 'detail'])->name('diary.detail');
-    Route::get('/edit/{id}', [DiaryController::class, 'edit'])->name('diary.edit');
-    Route::post('/create', [DiaryController::class, 'store'])->name('diary.store');
-    Route::PATCH('/update', [DiaryController::class, 'update'])->name('diary.update');
-    Route::delete('/destroy/{id}', [DiaryController::class, 'destroy'])->name('book.destroy');
+    Route::get('/detail/{id}', 'detail')->name('diary.detail');
+    Route::get('/edit/{id}', 'edit')->name('diary.edit');
+    Route::post('/create', 'store')->name('diary.store');
+    Route::PATCH('/update', 'update')->name('diary.update');
+    Route::delete('/destroy/{id}', 'destroy')->name('book.destroy');
 });
 
 require __DIR__.'/auth.php';
