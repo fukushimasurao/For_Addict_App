0e50407899fcb40dca12c10f21280d6b6058409c
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-11 15:54:09 +0900

    代表的なエラーページ作成

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <div class="py-3 sm:py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <section class="text-gray-600 body-font overflow-hidden">
+                        <div class="container px-5 py-4 mx-auto">
+                            <div class="flex flex-wrap -m-4 justify-around">
+                                <div class="flex p-4 my-2 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+                                    role="alert">
+                                    <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor"
+                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                    <div>
+                                        <span>404</span>
+                                        <span>ページが見つかりません🙇‍♂️</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
