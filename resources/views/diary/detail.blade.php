ad21ba4090f7ebab4349d0006307042d5969fdde
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-11 15:47:19 +0900

    text欄nl2brに変更

--- resources/views/diary/detail.blade.php
+++ resources/views/diary/detail.blade.php
@@ -39,16 +39,17 @@
                                 <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt class="text-sm font-medium text-gray-500">その時の気分感情</dt>
                                     <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                        <textarea id="coping_measures" name="coping_measures" rows="3" disabled placeholder="例：自分にがっかりした、、、"
-                                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">{{ $diary->feeling }}</textarea>
+                                        <div
+                                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                            {!! nl2br(e($diary->feeling)) !!}</div>
                                     </dd>
                                 </div>
                                 <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt class="text-sm font-medium text-gray-500">症状への対処法・反省点</dt>
-
                                     <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                        <textarea id="coping_measures" name="coping_measures" rows="3" disabled placeholder="例：パチ屋の見える道を歩かない！"
-                                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">{{ $diary->coping_measures }}</textarea>
+                                        <div
+                                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                            {!! nl2br(e($diary->coping_measures)) !!}</div>
                                     </dd>
                                 </div>
 
